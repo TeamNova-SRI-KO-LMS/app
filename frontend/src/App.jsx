ea0557c2bc56ea648277f2eafd472c306136d9bd
Mohottige.S.D. <146794439+Sahani-Mohottige@users.noreply.github.com>
2026-05-23 17:46:45 +0530

    feat: update Tailwind CSS and Vite integration
    - Added Tailwind CSS dependencies to package.json and package-lock.json.
    - Check whether tailwind works.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,11 @@ import './App.css'
 function App() {
   
   return (
-    <>
-       
-    </>
+      <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind Working test
+      </h1>
+    </div>
   )
 }
 
