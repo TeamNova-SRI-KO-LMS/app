8d54e117b76554a1c961094a782f4043acccfabd
Mohottige.S.D. <146794439+Sahani-Mohottige@users.noreply.github.com>
2026-06-19 15:54:39 +0530

    refactor: remove AdminAuthProvider wrapper and reorder imports

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,20 @@
 import "./index.css";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+
+// import { AdminAuthProvider } from './context/AdminAuthContext';
+import App from "./App.jsx";
+import AuthProvider from './context/AuthProvider';
 import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import AuthProvider from './context/AuthProvider';
-import { AdminAuthProvider } from './context/AdminAuthContext';
-import App from "./App.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <AuthProvider>
-        <AdminAuthProvider>
           <BrowserRouter>
             <App />
           </BrowserRouter>
-        </AdminAuthProvider>
       </AuthProvider>
     </GoogleOAuthProvider>
   </StrictMode>
