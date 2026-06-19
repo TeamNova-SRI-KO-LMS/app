4cf771aebafc5c1b2233555f904b235d13245217
Mohottige.S.D. <146794439+Sahani-Mohottige@users.noreply.github.com>
2026-06-19 15:48:35 +0530

    fix/add sample adminLayout file

---
+++ frontend/src/components/AdminLayout.jsx
@@ -0,0 +1,15 @@
+import Footer from './Footer';
+import Header from './Header';
+import { Outlet } from 'react-router-dom';
+
+export default function AdminLayout() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow pt-16">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
